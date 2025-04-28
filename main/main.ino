07a6e15968ca34a4995469f2723f9c7d0f3fd075
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2025-04-28 14:08:39 +0900

    writing main program

--- main/main.ino
+++ main/main.ino
@@ -15,13 +15,36 @@ IcsHardSerialClass krs(&Serial, EN_PIN, BAUDRATE, TIMEOUT);
 #define SERVO_LEFT 11
 
 Servo servo_vertical, servo_right, servo_left;
+Servo hold_servo[2]; // right, left
 
 #define RELAY_VOLTAGE A3
 #define RELAY_POLARITY A2
-#define RELAY_LEFT A1
 #define RELAY_RIGHT A0
+#define RELAY_LEFT A1
 
 #define LED 13
+
+// constant
+#define RIGHT 0
+#define LEFT 1
+#define BLACK 0
+#define WHITE 1
+
+// relay
+#define VOLTAGE_12V LOW
+#define VOLTAGE_5V HIGH
+const bool POLARITY[2][2] = {
+  {true, false}, // right black, right white
+  {false, true}  // left black, left white
+}
+
+// pwm servo deg
+const int HOLD_DEG_OPEN[2] = {50, 50};
+const int HOLD_DEG_UP[2] = {40, 40};
+const int HOLD_DEG_CLOSE[2] = {70, 70};
+#define VERTICAL_DEG_UP 40
+#define VERTICAL_DEG_BOARD 80
+#define VERTICAL_DEG_SUPPLY 50
 
 void setup() {
   // Software Serial for PC
@@ -32,20 +55,58 @@ void setup() {
 
   // PWM servo
   servo_vertical.attach(SERVO_VERTICAL);
-  servo_left.attach(SERVO_LEFT);
-  servo_right.attach(SERVO_RIGHT);
+  hold_servo[RIGHT].attach(SERVO_RIGHT);
+  hold_servo[LEFT].attach(SERVO_LEFT);
 
   // Relay for electromagnets
   pinMode(RELAY_VOLTAGE, OUTPUT);
   pinMode(RELAY_POLARITY, OUTPUT);
-  pinMode(RELAY_LEFT, OUTPUT);
   pinMode(RELAY_RIGHT, OUTPUT);
+  pinMode(RELAY_LEFT, OUTPUT);
 
   pinMode(LED, OUTPUT);
   
   Serial2.println("Start!");
   digitalWrite(LED, HIGH);
 }
+
+void move_vertical(int now_deg, int to_deg, int delay_microsec_per_deg) {
+  int deg_dif = abs(now_deg - to_deg);
+  int delta = 1;
+  if (to_deg < now_deg) {
+    delta = -1;
+  }
+  for (int deg = now_deg; deg != to_deg; deg += delta) {
+    servo_vertical.write(deg);
+    delayMicroseconds(delay_microsec_per_deg);
+  }
+}
+
+void hold_disc_board(int rl, int bw) {
+  digitalWrite(RELAY_RIGHT, LOW);
+  digitalWrite(RELAY_LEFT, LOW);
+  
+  hold_servo[rl].write(HOLD_DEG_OPEN[rl]);
+  hold_servo[rl ^ 1].write(HOLD_DEG_UP[rl ^ 1]);
+  delay(100);
+  move_vertical(VERTICAL_DEG_UP, VERTICAL_DEG_BOARD, 50);
+
+  digitalWrite(RELAY_VOLTAGE, VOLTAGE_12V);
+  digitalWrite(RELAY_POLARITY, POLARITY[rl][bw]);
+  if (rl == RIGHT) {
+    digitalWrite(RELAY_RIGHT, HIGH);
+    digitalWrite(RELAY_LEFT, LOW);
+  } else {
+    digitalWrite(RELAY_RIGHT, LOW);
+    digitalWrite(RELAY_LEFT, HIGH);
+  }
+}
+
+void move_arm(int x, int y, int delay_microsec_per_deg) {
+  
+}
+
+
 
 void loop() {
   servo_vertical.write(90);
